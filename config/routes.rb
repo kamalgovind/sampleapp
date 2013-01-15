dd51ff1c28fe10776bcfd8bb598cf0503a6f885e
kamalgovind <gkamal@gmail.com>
2013-01-15 10:53:27 +0530

    get it

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Sampleapp::Application.routes.draw do
 
   get "static_pages/help"
 
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -59,4 +61,5 @@ Sampleapp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
